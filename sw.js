dd90e72f015e20584717a86ec61049bf72216044
郧山鱼未眠 <2529855535@qq.com>
2026-05-21 15:57:44 +0800

    fix: lighten thinking section chrome

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump together with APP_VERSION to force old caches out.
-const SW_VERSION = "19";
+const SW_VERSION = "20";
 const CACHE_NAME = "moyu-v" + SW_VERSION;
 
 function cacheable(req, res) {
