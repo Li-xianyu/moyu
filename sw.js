387d8ef32bf4a97a34a094febc13eeffa20d17fb
郧山鱼未眠 <2529855535@qq.com>
2026-05-22 19:22:33 +0800

    fix: redesign mobile API settings layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump together with APP_VERSION to force old caches out.
-const SW_VERSION = "51";
+const SW_VERSION = "52";
 const CACHE_NAME = "moyu-v" + SW_VERSION;
 
 function cacheable(req, res) {
