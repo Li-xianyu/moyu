ae6980935bbe55d7c4597b39edf421006d4c79a0
郧山鱼未眠 <2529855535@qq.com>
2026-05-31 21:59:21 +0800

    fix: duplicate tts block removed, correct position

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump together with APP_VERSION to force old caches out.
-const SW_VERSION = "2026.5.31.1";
+const SW_VERSION = "2026.5.31.3";
 const CACHE_NAME = "moyu-v" + SW_VERSION;
 
 function cacheable(req, res) {
